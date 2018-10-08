a1bd1a316ac7c34127aec6cf3d6aebd73823e246
hyang <yhcd028@gmail>
2018-10-08 10:27:43 +0800

    fix install

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
+#!/bin/bash 
 [ -f /etc/wsl.conf ] || {
   sudo cp $PWD/etc.wsl.conf /etc/wsl.conf
 }
-[ $(which zsh) == "" ] && {
+[ "$(which zsh)" == "" ] && {
   sudo apt-get install zsh
   zsh
   sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-  echo source $PWD/zshrc >> ~/.zshrc
 }
+echo source $PWD/zshrc >> ~/.zshrc
